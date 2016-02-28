38d59fb11270ceb3577d3cb3641bd5fcca509956
Vishal Erabelli <vishalxv@gmail.com>
2016-02-28 04:34:45 -0500

    CopyRJava now only copies relevant constants
    It now copies only the (raw, drawable) resource constants
    It can be modified to copy over more classes as needed

--- misc/src/main/java/com/example/CopyRJava.java
+++ misc/src/main/java/com/example/CopyRJava.java
@@ -4,23 +4,57 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
 public class CopyRJava {
     public static void main(String[] args) {
-        String src = "app/build/generated/source/r/debug/io/github/simcards/simcards/R.java";
-        String dst = "desktop/src/main/java/io/github/simcards/desktop/R.java";
+        // the path of the R.java file we want to copy
+        Path src = Paths.get("app/build/generated/source/r/debug/io/github/simcards/simcards/R.java");
+        // the path of the new R.java file
+        Path dst = Paths.get("desktop/src/main/java/io/github/simcards/desktop/R.java");
+        // the names of the class constants we want to copy
+        String[] classNames = new String[]{"raw", "drawable"};
+
+
+        Charset charset = StandardCharsets.UTF_8;
+        String content;
         try {
-            Charset charset = StandardCharsets.UTF_8;
-            String content = new String(Files.readAllBytes(Paths.get(src)), charset);
-            content = content.replaceAll("package io.github.simcards.simcards", "package io.github.simcards.desktop");
-            Files.write(Paths.get(dst), content.getBytes(charset));
+            content = new String(Files.readAllBytes(src), charset);
         } catch (IOException e) {
             System.out.println("CopyRJava failed...");
             e.printStackTrace();
             return;
         }
+
+        StringBuilder builder = new StringBuilder();
+        builder.append("package io.github.simcards.desktop;\n\n");
+        builder.append("public final class R {\n");
+        builder = getClasses(content, builder, classNames);
+        builder.append("}\n");
+        System.out.println(builder.toString());
+
+        try {
+            Files.write(dst, builder.toString().getBytes(charset));
+        } catch (IOException e) {
+            System.out.println("CopyRJava failed...");
+            e.printStackTrace();
+            return;
+        }
+
         System.out.println("CopyRJava succeeded");
+    }
+
+    public static StringBuilder getClasses(String content, StringBuilder builder, String[] classes) {
+        for (int i = 0; i < classes.length; i++) {
+            int begin_ind = content.indexOf("public static final class " + classes[i]);
+            int end_ind = content.indexOf('}', begin_ind);
+            String classStr = content.substring(begin_ind, end_ind + 1);
+            builder.append("\t");
+            builder.append(classStr);
+            builder.append("\n");
+        }
+        return builder;
     }
 }
